0f4e51e1c605d8dd28acf9967fbd6c5219dd7900
mmd <abusemmd@gmail.com>
2024-05-28 20:20:47 +0330

    splited in tunnel folder / experimental port80 listener / new flags and some flags renamed

--- main.go
+++ main.go
@@ -1,192 +1,40 @@
 package main
 
 import (
-	"crypto/tls"
 	"flag"
-	"fmt"
-	"io"
-	"log"
-	"net"
-	"strings"
-	"sync"
 
-	"github.com/hashicorp/yamux"
+	"mmd/tlstun/tunnel"
 )
-
-func proxy(conn1, conn2 net.Conn) {
-
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	defer conn1.Close()
-	defer conn2.Close()
-
-	go func() {
-		defer wg.Done()
-		io.Copy(conn1, conn2)
-	}()
-	go func() {
-		defer wg.Done()
-		io.Copy(conn2, conn1)
-	}()
-
-	wg.Wait()
-}
-
-type Srv struct {
-	Laddr  string
-	Passwd string
-}
-
-var ptol = make(map[string]net.Listener)
-
-func handle_srv_listener(conn net.Conn, passwd string) {
-
-	authBuff := make([]byte, 1000)
-	conn.Read(authBuff)
-	hello_buff := strings.Split(string(authBuff), "_")
-	if hello_buff[0] == passwd {
-		session, err := yamux.Client(conn, yamux.DefaultConfig())
-		if err != nil {
-			log.Fatalf("failed start yamux client: %s", err)
-
-		}
-		l, ok := ptol[hello_buff[1]]
-		if ok {
-			l.Close()
-		}
-		listener, err := net.Listen("tcp", "0.0.0.0:"+hello_buff[1])
-		if err != nil {
-			fmt.Printf("err raised %s", err)
-			return
-		}
-
-		ptol[hello_buff[1]] = listener
-
-		for {
-
-			outerconn, err := listener.Accept()
-			if err != nil {
-				log.Printf("server: accept: 11  %s", err)
-				listener.Close()
-				break
-			}
-			stream, err := session.Open()
-			if err != nil {
-				listener.Close()
-				break
-
-			}
-
-			go proxy(outerconn, stream)
-
-		}
-	}
-}
-
-func (s Srv) strat_listener() {
-	cert, err := tls.LoadX509KeyPair("tls.cert", "tls.key")
-	if err != nil {
-		log.Printf("err: %s", err)
-	}
-	conf := tls.Config{
-		Certificates: []tls.Certificate{cert},
-	}
-
-	listener, err := net.Listen("tcp", s.Laddr)
-	if err != nil {
-		log.Fatalf("server: listen: %s", err)
-	}
-	for {
-
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("server: accept: %s", err)
-
-		}
-
-		tlsConn := tls.Server(conn, &conf)
-		go handle_srv_listener(tlsConn, s.Passwd)
-	}
-}
-
-type Cli struct {
-	RemoteAddr  string
-	ExposedPort string
-	Passwd      string
-	V2p         string
-}
-
-func (c Cli) start_cli() {
-	conf := tls.Config{
-		InsecureSkipVerify: true,
-	}
-
-	conn, err := net.Dial("tcp", c.RemoteAddr)
-	if err != nil {
-		log.Fatalf("failed: %s", err)
-	}
-	tlsConn := tls.Client(conn, &conf)
-	tlsConn.Write([]byte(fmt.Sprintf("%s_%s_", c.Passwd, c.ExposedPort)))
-
-	sesssion, err := yamux.Server(tlsConn, yamux.DefaultConfig())
-	if err != nil {
-		log.Fatalf("failed: %s", err)
-
-	}
-	for {
-		stream, err := sesssion.Accept()
-		if err != nil {
-			log.Fatalf("failed: %s", err)
-
-		}
-		destconn, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%s", c.V2p))
-		if err != nil {
-			log.Fatalf("failed: %s", err)
-
-		}
-		go proxy(destconn, stream)
-	}
-
-}
 
 func main() {
 	mode := flag.String("m", "s", "mode ")
-	v2port := flag.String("v2p", "1080", "port")
-	eport := flag.String("ep", "5012", "port")
+	clibckport := flag.String("bckp", "1080", "port")
+	cliexposedport := flag.String("ep", "5012", "port")
+	tlscert := flag.String("cert", "tls.cert", "tls certificate")
+	tlskey := flag.String("key", "tls.key", "tls key")
 	passwd := flag.String("passwd", "123456", "passwd")
-	addr := flag.String("addr", "0.0.0.0:4443", "addr")
+	srvaddr := flag.String("addr", "0.0.0.0:4443", "addr")
 
 	flag.Parse()
 	if *mode == "s" {
 
-		// s := Srv{
-		// 	Laddr:  "0.0.0.0:8000",
-		// 	Passwd: "hello",
-		// }
-
-		s := Srv{
-			Laddr:  *addr,
-			Passwd: *passwd,
+		s := tunnel.Srv{
+			Laddr:   *srvaddr,
+			Passwd:  *passwd,
+			Tlscert: *tlscert,
+			Tlskey:  *tlskey,
 		}
-		s.strat_listener()
+		s.StartL()
 
 	} else if *mode == "c" {
 
-		// c := Cli{
-		// 	RemoteAddr:  "127.0.0.1:8000",
-		// 	ExposedPort: "5012",
-		// 	V2p:         "1080",
-		// 	Passwd:      "hello",
-		// }
-
-		c := Cli{
-			RemoteAddr:  *addr,
-			ExposedPort: *eport,
-			V2p:         *v2port,
-			Passwd:      *passwd,
+		c := tunnel.Cli{
+			RemoteAddr: *srvaddr,
+			ExposePort: *cliexposedport,
+			Bckp:       *clibckport,
+			Passwd:     *passwd,
 		}
-		c.start_cli()
+		c.StartCli()
 
 	}
 }
