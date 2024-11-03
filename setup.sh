c1effb1e83bbfafa90c23ba523e46715f175362a
mmd <abusemmd@gmail.com>
2024-11-03 14:14:29 +0330

    setup script

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo cp tlstun /usr/bin 
+sudo cp tls.cert / 
+sudo cp tls.key / 
+
+echo enter addr 
+read addr 
+
+echo enter passwd 
+read passwd
+
+cat > /etc/systemd/system/tlstun.service <<EOF
+[Unit]
+Description=tls tunnel
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+ExecStart=tlstun -addr $addr  -passwd $passwd
+Restart=always
+RestartSec=60s
+User=root
+
+[Install]
+WantedBy=default.target
+EOF 
+
+
+sudo systemctl daemon-reload
+sudo systemctl start tlstun.service 
